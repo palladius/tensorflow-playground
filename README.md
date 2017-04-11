1918a1aba31d2378d089fd1a3c7eb1a5ad883707
Riccardo Carlesso <palladiusbonton+derek.zrh.corp.google.com@gmail.com>
2017-04-11 14:17:44 +0200

    4 gives me error :/

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 My playground with TF. Basically jkust following the public docs and dumping code here...
 
-Along with usual StackOverflow perfectionisms :)
+Docs from: https://www.tensorflow.org/get_started/get_started
 
+Along with usual StackOverflow perfectionisms :)
--- _common.py
+++ _common.py
@@ -1,6 +1,18 @@
-#_common.py
+"""Common stuff.
+
+Great article on reflection: http://stackoverflow.com/questions/2654113/python-how-to-get-the-callers-method-name-in-the-called-method
+"""
 
 
-
+import inspect
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2' # to remove the warnings
+
+
+
+
+if __name__ == '__main__':
+	print "I'm just a library - please don't execute me again or I'll go to lib/HR.py"
+else:
+	print "Thanks for trating me as a library. I'm NOT an object, or from Maine! Called by: '{}'".format(inspect.stack()[1][1])
+	#print 'caller name: ', inspect.stack()[1]
